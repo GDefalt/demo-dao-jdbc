79029d0d16ea233584403514cb190002c5c7cc07
GDefalt <bluegamer2627@hotmail.com>
2025-07-17 18:22:23 -0300

    SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import dao.DaoFactory;
+import dao.SellerDao;
+import dao.impl.SellerDaoJDBC;
 import entities.Department;
 import entities.Seller;
 
@@ -12,6 +15,10 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, 3000.0,  LocalDate.parse("01/06/2006", formatter), "bob@gmail.com", "Bob", new Department(1, "Books"));
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
+
+
     }
 }
